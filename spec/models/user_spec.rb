2ea24c0b8927c828dd7c86640c7fcbb662d7ff58
fumiya-yoshii <135097414+fumiya-yoshii@users.noreply.github.com>
2023-06-23 18:39:07 +0900

    itメソッドを用いてテストの状況を記述しましょう

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    # ユーザー新規登録についてのテストコードを記述します
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+      user = User.new(nikname: ``, email: `test@example`, password:  '000000', password_confirmation: '000000')
+    end
+    it 'emailが空では登録できない' do
+      # emailが空では登録できないテストコードを記述します
+    end
+  end
 end
